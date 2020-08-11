e4502d681a8d2a92e1f5c6c6ede8a32bd7e3c033
Dmitry <purpl3mint@yandex.ru>
2020-08-12 01:30:16 +0700

    Added scroll for blogs and fixed title

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 
-const pug2html = require('./gulp/pug2html');
-const styles = require('./gulp/styles');
-const images = require('./gulp/images');
+const pug2html = require("./gulp/pug2html");
+const styles = require("./gulp/styles");
+const images = require("./gulp/images");
+const scripts = require("./gulp/scripts");
 
-module.exports.start = gulp.series(pug2html, styles, images);
+module.exports.start = gulp.series(pug2html, styles, images, scripts);
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
-let blogScroll = document.getElementsById("blog-range");
-let blogSize = document.querySelector(".main__blog-articles").offsetWidth;
+const changeRange = () => {
+  const blogRange = document.getElementById("blog-range");
+  const blogWidth = document.querySelector(".main__blog-articles").offsetWidth;
+
+  console.log(blogRange);
+
+  blogRange.addEventListener("input", () => {
+    const screenWidth = document.body.clientWidth;
+    const rangeValue = blogRange.value;
+    const mainBlogArticles = document.querySelector(".main__blog-articles");
+    const offset = Math.floor(
+      (rangeValue * (blogWidth - screenWidth + 50)) / 100
+    );
+
+    console.log(offset);
+
+    mainBlogArticles.style.left = "-" + offset + "px";
+  });
+};
+
+changeRange();
